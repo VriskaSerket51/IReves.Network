6bd1191a40f3161f9a3f9f68fa8aa4c33747ad30
Robert Lodico <lodico.rj@gmail.com>
2020-12-23 06:33:09 -0500

    Updated copyright, multitarget project

--- Lidgren.Network/Properties/AssemblyInfo.cs
+++ Lidgren.Network/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Lidgren.Network")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright lidgren © 2015-2020, AscensionGameDev 2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
